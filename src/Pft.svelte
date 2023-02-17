39d3910c1b6ff17f481d67712da386503c3adc0d
Kang <thisispiggy@gmail.com>
2023-02-17 12:39:39 -0500

    exported prompts

--- src/Pft.svelte
+++ src/Pft.svelte
@@ -1,5 +1,16 @@
 <script>
   import * as d3 from "d3";
+  import {
+    effortPrompt,
+    spirometryPrompt,
+    bronchPrompt,
+    flowPrompt,
+    volumePrompt,
+    hyperPrompt,
+    diffusingPrompt,
+    diffusingCorrectPrompt,
+    conclusionPrompt,
+  } from "./lib/prompts";
 
   let FEV1, FVC, FEVFVC, TLC, RV, RVTLC, DLCOunc, DLCOcor;
   let effort,
@@ -13,75 +24,6 @@
     hyper,
     signature;
   let inputText, formattedData;
-
-  const effortPrompt = [
-    "",
-    "Adequate effort, results reproducible.",
-    "Inadequate effort, results are not reproducible.",
-    "FVC maneuver did not extend beyond 6 seconds.",
-  ];
-  const spirometryPrompt = [
-    "",
-    "Normal spirometry.",
-    "Mild obstructive defect based on FEV1.",
-    "Moderate obstructive defect based on FEV1. ",
-    "Moderately severe obstructive defect based on FEV1. ",
-    "Severe obstructive defect based on FEV1. ",
-    "Very severe obstructive defect based on FEV1. ",
-    "Vital capacity is reduced. This may be due to submaximal effort, air trapping, a restrictive ventilatory defect, or a nonspecific defect. Consider measurement of total lung capacity and postbronchodilator spirometry if clinically indicated. ",
-    "Restrictive lung defect suggested on spirometry. Consider lung volume studies to confirm this. ",
-    "Vital capacity is reduced. This may be due to submaximal effort, air trapping, a restrictive ventilatory defect, or a nonspecific defect.",
-  ];
-  const bronchPrompt = [
-    "",
-    "There is a significant response to bronchodilator inhalation based on FEV1 and FVC. ",
-    "There is no significant response to bronchodilator inhalation; however, this does not preclude benefit from use of a bronchodilator.",
-  ];
-  const flowPrompt = [
-    "",
-    "Flow-volume loop does not suggest any fixed airway obstruction.",
-    "Flow-volume loop is suggestive of a variable intrathoracic  obstruction. Consider direct airway inspection if clinically appropriate.",
-    "Flow-volume loop is suggestive of a  variable extrathoracic  obstruction. Consider direct airway inspection if clinically appropriate.",
-    "Flow-volume loop is suggestive of a  fixed obstruction. Consider direct airway inspection if clinically appropriate.",
-  ];
-  const volumePrompt = [
-    "",
-    "Normal lung volumes.",
-    "Mild restrictive defect based on reduced TLC and FEV1.",
-    "Moderate restrictive defect based on reduced TLC and FEV1.",
-    "Moderately severe restrictive defect based on reduced TLC and FEV1.",
-    "Severe restrictive defect based on reduced TLC and FEV1.",
-    "Very severe restrictive defect based on reduced TLC and FEV1.",
-  ];
-  const hyperPrompt = [
-    "",
-    "Increased TLC indicates hyperinflation.",
-    "Increased RV and RV/TLC indicates air trapping.",
-  ];
-  const diffusingPrompt = [
-    "",
-    "Normal diffusing capacity.",
-    "Mild reduction in diffusing capacity.",
-    "Moderate reduction in diffusing capacity.",
-    "Severe reduction in diffusing capacity.",
-    "Diffusing capacity is elevated.",
-  ];
-  const diffusingCorrectPrompt = [
-    "",
-    "The diffusing capacity is corrected using hemoglobin.",
-    "The diffusing capacity is uncorrected.",
-  ];
-  const conclusionPrompt = [
-    "",
-    "Normal PFTs.",
-    "Obstructive defect is indicated on this PFT.",
-    "Restrictive defect is indicated on this PFT.",
-    "Combined obstructive and restrictive defect is indicated on this PFT.",
-    "No evidence of airflow obstruction or restrictive disease. Reduced FEV1 and FVC with normal lung volumes represent a non-specific pattern.",
-    "Restrictive defect suggested by spirometry. Recommend lung volume studies to confirm this.",
-    "Clinical correlation suggested.",
-    "Normal spirometry.",
-  ];
 
   function setVariables() {
     formattedData = inputText.replace(/^\s*\n/gm, "");
