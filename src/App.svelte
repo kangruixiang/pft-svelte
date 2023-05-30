56faa779553219b0854dc8101f73d06fa4815fda
Kang <thisispiggy@gmail.com>
2023-05-29 23:33:16 -0400

    fixed mobile padding

--- src/App.svelte
+++ src/App.svelte
@@ -204,7 +204,7 @@
 
 <main class="flex items-center min-h-screen justify-center">
   <div
-    class="container max-w-6xl mx-4 p-12 my-12 rounded-lg lg:p-12 bg-zinc-100"
+    class="container max-w-6xl mx-2 px-2 py-4 md:mx-4 md:p-12 md:my-12 rounded-lg lg:p-12 bg-zinc-100"
   >
     <h1>PFT Reader - Algorithmic Approach to Pulmonary Function Test</h1>
     <div class="grid lg:grid-cols-2 gap-4">
